2be363059e8591dd5579fa468ff55188333f72b0
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-04-28 09:27:07 +0200

    Script working correctly. It would be nice to add instructions step by step

--- script.js
+++ script.js
@@ -1,149 +1,75 @@
 let humanScore = 0;
 let computerScore = 0;
-let computerSelection = getComputerChoice();
-let humanSelection = getHumanChoice();
-
-
-// function to get random rock paper scissors from computer
-
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"]
-    let result = Math.floor(Math.random() * options.length);
-    
-    if(result == 0) {
-        return options[0]
-    } else if (result == 1) {
-        return options[1]
-    } else if (result == 2) {
-        return options[2]
-    }
-}
-
-// function to get prompt from user
-
-function getHumanChoice() {
-    let choice = window.prompt("Choose between rock, paper or scissors").toLowerCase();
-    while (choice != "rock" && choice != "paper" && choice != "scissors"){
-    choice = window.prompt("Choose between rock, paper or scissors").toLowerCase();
-    }
-    return choice;
-}
 
 
 // function round
-function playRound(humanChoice, computerChoice) {
+function playRound() {
 
+    let computerSelection = getComputerChoice();
+    let humanSelection = getHumanChoice();
 
-    
-    
-    if(humanChoice == computerChoice) {
-        return console.log("Empate!!!!")
-    } else if(humanChoice == "rock" && computerChoice == "scissors") {
+    function getComputerChoice() {
+        const options = ["rock", "paper", "scissors"]
+        let result = Math.floor(Math.random() * options.length);
+        
+        if(result == 0) {
+            return options[0]
+        } else if (result == 1) {
+            return options[1]
+        } else if (result == 2) {
+            return options[2]
+        }
+    }
+
+    function getHumanChoice() {
+        let choice = window.prompt("Play rock, paper or scissors a minimun of 5 times!!").toLowerCase();
+        while (choice != "rock" && choice != "paper" && choice != "scissors"){
+        choice = window.prompt("You didn't choose correctly. Choose again between: Rock, Paper or Scissors.").toLowerCase();
+        }
+        return choice;
+    }
+
+    if(computerSelection == humanSelection) {
+        return console.log("Empate")
+    } else if(humanSelection == "rock" && computerSelection == "scissors") {
         humanScore++
         return console.log("You win with rock to scissors!!")
-    } else if(humanChoice == "rock" && computerChoice == "paper") {
+    } else if(humanSelection == "rock" && computerSelection == "paper") {
         computerScore++
         return console.log("Computer wins to you with paper")
-    } else if(humanChoice == "paper" && computerChoice == "rock") {
+    } else if(humanSelection == "paper" && computerSelection == "rock") {
         humanScore++
         return console.log("You win with paper to rock")
-    } else if(humanChoice == "paper" && computerChoice == "scissors") {
+    } else if(humanSelection == "paper" && computerSelection == "scissors") {
         computerScore++
         return console.log("Computer wins to you with scissors")
-    } else if(humanChoice == "scissors"&& computerChoice == "paper") {
+    } else if(humanSelection == "scissors"&& computerSelection == "paper") {
         humanScore++
         return console.log("You win with scissors to paper")
-    } else if(humanChoice == "scissors"&& computerChoice == "rock") {
+    } else if(humanSelection == "scissors"&& computerSelection == "rock") {
         computerScore++
         return console.log("Computer wins to you with rock")
     } 
 }
-
-playRound(computerSelection, humanSelection)
-playRound(computerSelection, humanSelection)
-
-
-console.log(humanScore)
-console.log(computerScore)
-console.log(humanSelection)
-console.log(computerSelection)
-
-
-/*
-
-let humanScore = 0;
-let computerScore = 0;
-
-// function to get prompt from user
-
-function getHumanChoice() {
-    let seleccion = prompt("Choose: Rock, Paper or Scissors")
-    return seleccion.toLowerCase();
-}
-
-console.log(getHumanChoice());
-
-// function to get random rock paper scissors from computer
-
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"]
-    let result = Math.floor(Math.random() * options.length);
-    
-    if(result == 0) {
-        return options[0]
-    } else if (result == 1) {
-        return options[1]
-    } else if (result == 2) {
-        return options[2]
-    }
-}
-
-// function round
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice == computerChoice) {
-        return console.log("Empate!!!!")
-    } else if(humanChoice == "rock" && computerChoice == "scissors") {
-        humanScore++
-        return console.log("You win with rock to scissors!!")
-    } else if(humanChoice == "rock" && computerChoice == "paper") {
-        computerScore++
-        return console.log("Computer wins to you with paper")
-    } else if(humanChoice == "paper" && computerChoice == "rock") {
-        humanScore++
-        return console.log("You win with paper to rock")
-    } else if(humanChoice == "paper" && computerChoice == "scissors") {
-        computerScore++
-        return console.log("Computer wins to you with scissors")
-    } else if(humanChoice == "scissors"&& computerChoice == "paper") {
-        humanScore++
-        return console.log("You win with scissors to paper")
-    } else if(humanChoice == "scissors"&& computerChoice == "rock") {
-        computerScore++
-        return console.log("Computer wins to you with rock")
-    } 
-
-    
-}
-
-// var for playround
-
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-
-// function game
 
 function playGame() {
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
-    playRound(humanSelection, computerSelection);
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+
+    if(humanScore == computerScore) {
+        console.log("Empate!!!")
+    } else if(humanScore < computerScore) {
+        console.log("Computer wins!!")
+    } else if(humanScore > computerScore) {
+        console.log("You win!!!!")
+    }
+
+
+    console.log("Human: " + humanScore)
+    console.log("Computer: " + computerScore)
 }
 
-playGame();
-
-console.log(humanScore)
-console.log(computerScore)
-
-*/
+playGame()
